6071d376b1da76b6fe4bdd244119459da3ebe864
Hubert Kopania <niutka.gekon@gmail.com>
2025-06-12 10:29:11 +0200

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Main(){
+	return (
+		<div className="w-full h-full flex justify-center align-middle dark:text-white">
+			<div className="border dark:border-white border-black">
+				<h1 className="text-4xl font-bold">
+					Welcom to JumpCHAT
+				</h1>
+				<h2 className="text-2xl">
+					New level of chat privacy
+				</h2>
+				<Button></Button>
+			</div>
+		</div>
+	)
+}
