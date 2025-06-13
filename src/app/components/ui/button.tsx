7bbc39897e3a0cb9d31e0c83c9bd7dacd75c95b6
Hubert Kopania <niutka.gekon@gmail.com>
2025-06-13 21:57:56 +0200

    Landing page

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -1,3 +1,8 @@
-export default function Button({children}){
-
+import { Children, ReactNode } from "react";
+export default function Button(props: {children: ReactNode}){
+    return (
+        <button className="p-3 m-3 border dark:border-white border-black hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-black duration-200">
+            {props.children}
+        </button>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import Button from "./components/ui/button";
+
 export default function Main(){
 	return (
-		<div className="w-full h-full flex justify-center align-middle dark:text-white">
-			<div className="border dark:border-white border-black">
+		<div className="w-full h-screen flex justify-center align-center dark:text-white">
+			<div className="border dark:border-white border-black p-5 m-auto">
 				<h1 className="text-4xl font-bold">
 					Welcom to JumpCHAT
 				</h1>
 				<h2 className="text-2xl">
 					New level of chat privacy
 				</h2>
-				<Button></Button>
+				<Button>
+					Start chatting
+				</Button>
 			</div>
 		</div>
 	)
